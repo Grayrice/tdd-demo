76fc8cac8e484b3eb4c08637b4954c9365854454
刘鹏飞 <1033069491@qq.com>
2022-08-15 00:02:44 +0800

    添加问题查找jpa单元测试

--- src/test/java/com/demo/study/domain/repository/QuestionRepositoryTest.java
+++ src/test/java/com/demo/study/domain/repository/QuestionRepositoryTest.java
@@ -6,11 +6,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import javax.persistence.EntityManager;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 @DataJpaTest
 class QuestionRepositoryTest {
+
+    @Autowired
+    private EntityManager entityManager;
 
     @Autowired
     private QuestionRepository questionRepository;
@@ -20,8 +25,22 @@ class QuestionRepositoryTest {
         Question question = new Question("uuid_question_001", "a test title", "a test detail");
         Question savedQuestion = questionRepository.save(question);
         assertThat(savedQuestion.getId(), Matchers.is(Matchers.notNullValue()));
-        assertThat(savedQuestion.getQuestionId(), equalTo(question.getQuestionId()));
-        assertThat(savedQuestion.getTitle(), equalTo(question.getTitle()));
-        assertThat(savedQuestion.getDetail(), equalTo(question.getDetail()));
+        assertSameQuestion(savedQuestion, question);
+    }
+
+    @Test
+    void repository_should_successful_find_question_by_id(){
+        Question question = new Question("uuid_question_001", "a test title", "a test detail");
+        Question savedQuestion = questionRepository.saveAndFlush(question);
+        entityManager.detach(savedQuestion);
+        Question foundQuestion = questionRepository.findById(savedQuestion.getId()).orElseThrow(AssertionError::new);
+        assertThat(foundQuestion.getId(), Matchers.is(Matchers.notNullValue()));
+        assertSameQuestion(foundQuestion, savedQuestion);
+    }
+
+    private void assertSameQuestion(Question actualQuestion, Question expectedQuestion) {
+        assertThat(actualQuestion.getQuestionId(), equalTo(expectedQuestion.getQuestionId()));
+        assertThat(actualQuestion.getTitle(), equalTo(expectedQuestion.getTitle()));
+        assertThat(actualQuestion.getDetail(), equalTo(expectedQuestion.getDetail()));
     }
 }
